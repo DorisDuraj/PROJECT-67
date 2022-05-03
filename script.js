944f39dfd2c87c8c83f7ea76c480e4a842312527
DorisDuraj <104841851+DorisDuraj@users.noreply.github.com>
2022-05-03 09:53:25 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+$(".reply-Enter").dblclick(function() {
+   $(".Enter").toggle();
+});
+
+$(".reply-Door1").click(function() {
+   $(".door#1").slideDown();
+});
+
+$(".reply-Door2").click(function() {
+   $(".Door#2").slideDown();
+});
+
+$(".reply-Enter").click(function() {
+   $(".Enter").toggle();
+});
+
+$(".reply-Enter").click(function() {
+   $(".Enter").toggle();
+});
